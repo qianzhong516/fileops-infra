4df3387ae28d09b28695261d55d500ca6d1ec507
qianzhong516 <ads.gutless810@passinbox.com>
2026-05-13 19:12:44 +0800

    let helm create namespace if not exist

--- helm/main.tf
+++ helm/main.tf
@@ -1,8 +1,9 @@
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  namespace  = "argocd"
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
 
   # values = [
   #   file("${path.module}/argocd-values.yml")
