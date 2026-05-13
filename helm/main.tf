a4e484cb2d0fb5bade6050552bec8bb735b1266b
qianzhong516 <ads.gutless810@passinbox.com>
2026-05-13 19:47:25 +0800

    add todos

--- helm/main.tf
+++ helm/main.tf
@@ -1,8 +1,10 @@
+# TODO: separate CRD from chart installation to counter the problem of undeleted CRDs after a namespace switch. 
+# Reference: https://github.com/argoproj/argo-helm#custom-resource-definitions
 resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  namespace        = "argo-cd"
+  namespace        = "argocd"
   create_namespace = true
 
   # values = [
