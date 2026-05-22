d973f2ae0486c875c9efbb2ebccf0e51bcc7a192
qianzhong516 <ads.gutless810@passinbox.com>
2026-05-22 22:32:00 +0800

    add kms key for SOPS

--- helm/argocd-repo-server.tf
+++ helm/argocd-repo-server.tf
@@ -54,3 +54,78 @@ resource "aws_iam_policy" "argocd_repo_server" {
     ]
   })
 }
+
+# SOPS key for encrypting Kubernetes secrets
+resource "aws_kms_alias" "sops_key_alias" {
+  name          = "alias/fileops-dev"
+  target_key_id = aws_kms_key.sops_key.key_id
+}
+
+resource "aws_kms_key" "sops_key" {
+  description             = "An symmetric encryption KMS key for SOPs"
+  enable_key_rotation     = true
+  deletion_window_in_days = 7
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "key-default-1"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = [
+            "arn:aws:iam::665303624691:role/tf_oidc_role", "arn:aws:iam::665303624691:user/eks-training-admin"
+          ]
+        },
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowArgoCDRepoServerDecrypt"
+        Effect = "Allow"
+        Principal = {
+          AWS = [
+            aws_iam_role.argocd_repo_server.arn
+          ]
+        }
+        Action = [
+          "kms:Decrypt",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      }
+      # {
+      #   Sid    = "Allow administration of the key"
+      #   Effect = "Allow"
+      #   Principal = {
+      #     AWS = "arn:aws:iam::665303624691:user/eks-training-admin"
+      #   },
+      #   Action = [
+      #     "kms:ReplicateKey",
+      #     "kms:Create*",
+      #     "kms:Describe*",
+      #     "kms:Enable*",
+      #     "kms:List*",
+      #     "kms:Put*",
+      #     "kms:Update*",
+      #     "kms:Revoke*",
+      #     "kms:Disable*",
+      #     "kms:Get*",
+      #     "kms:Delete*",
+      #     "kms:ScheduleKeyDeletion",
+      #     "kms:CancelKeyDeletion"
+      #   ],
+      #   Resource = "*"
+      # }
+    ]
+  })
+  tags = merge(local.tags, {
+    Name        = "fileops-sops-master-key"
+    Environment = "dev"
+  })
+}
