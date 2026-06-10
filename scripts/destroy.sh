bd3509a82a0928d2185ed2ecd19ebba00e6160a6
qianzhong516 <ads.gutless810@passinbox.com>
2026-06-10 17:11:23 +0800

    add follow-up steps to destroy

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -13,18 +13,24 @@ done
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
-# TODO: Enable ArgoCD commands after the GRPC server is properly set up
-# if argocd app get fileops-dev >/dev/null 2>&1; then
-# 	echo "Destorying the app workloads..."
-# 	argocd app delete fileops-dev --cascade
-# 	echo "Destorying the app workloads is complete"
-# fi
-# if argocd app get argo-infra >/dev/null 2>&1; then
-# 	echo "Destorying the argocd infra workloads..."
-# 	argocd app delete argo-infra --cascade
-# 	echo "Destorying the argocd infra is complete."
-# fi
+# Delete Argocd apps
+# echo "Logging into Argocd Server..."
+# password=$(argocd admin initial-password -n argocd | head -n 1)
+# argocd login argocd.filesops.com --username "admin" --password "$password"
+# echo "Login is complete."
 
+# argo_apps=("karpenter-infra" "fileops-dev" "argocd-infra")
+# for app in "${argo_apps[@]}"; do
+# 	if argocd app get "$app" >/dev/null 2>&1; then
+# 		echo "Destorying $app workloads..."
+# 		argocd app set "$app" --source-position 1 --sync-policy none
+# 		argocd app delete "$app" --cascade -y
+# 		# TODO: Waiting for ALBs to be deleted
+# 		echo "Destorying $app workloads is complete"
+# 	fi
+# done
+
+# Delete Terraform workloads
 cd "$ROOT_DIR"/workloads
 echo "Destorying the workload resources..."
 terraform destroy --auto-approve
@@ -44,3 +50,10 @@ cd "$ROOT_DIR"/cluster
 echo "Destorying the cluster resources..."
 terraform destroy --auto-approve
 echo "Destorying the cluster resources is complete."
+
+cat <<EOF
+==========================================================================
+Congratulations! Deletion is successful. Please perform the following steps next:
+1) Remove the prometheus alert manager volume from your AWS account
+==========================================================================
+EOF
